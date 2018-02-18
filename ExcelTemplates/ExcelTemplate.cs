4c033d970e27fdb62e8e5923ccacd4d4a794e238
Куксенков Андрей <neo.qlive@gmail.com>
2018-02-18 14:48:47 +0300

    Добавьте файлы проекта.

---
+++ ExcelTemplates/ExcelTemplate.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OfficeOpenXml;
+
+namespace ExcelTemplates
+{
+    public sealed class ExcelTemplate
+    {
+        private readonly ExcelPackage _xlPackage;
+
+        public ExcelTemplate(string file)
+        {
+            FileInfo xlTemplate = new FileInfo(file);
+            _xlPackage = new ExcelPackage(xlTemplate);
+        }
+
+        public byte[] GetAsByteArray()
+        {
+            return _xlPackage.GetAsByteArray();
+        }
+
+        public void SetField(string field, string value)
+        {
+            foreach (var ws in _xlPackage.Workbook.Worksheets)
+            {
+                if (ws.Names.ContainsKey(field))
+                {
+                    ws.Names[field].Value = value;
+                    ws.Names[field].AutoFitColumns();
+                }
+            }
+
+            if (_xlPackage.Workbook.Names.ContainsKey(field))
+            {
+                _xlPackage.Workbook.Names[field].Value = value;
+                _xlPackage.Workbook.Names[field].AutoFitColumns();
+            }
+        }
+
+        public void SetField(string field, IEnumerable<string> values)
+        {
+            ExcelRangeBase range = null;
+            var namesCollection = _xlPackage.Workbook.Names;
+
+            if (namesCollection.ContainsKey(field))
+            {
+                var startRow = namesCollection[field].Start.Row;
+                var wsIndex = namesCollection[field].Worksheet.Index;
+
+                _xlPackage.Workbook.Worksheets[wsIndex].InsertRow(startRow + 1, values.Count());
+                range = namesCollection[field].LoadFromCollection(values, true, OfficeOpenXml.Table.TableStyles.Medium16);
+            }
+
+            foreach (var ws in _xlPackage.Workbook.Worksheets)
+            {
+                if (ws.Names.ContainsKey(field))
+                {
+                    var data = ws.Names[field].LoadFromCollection(values, true, OfficeOpenXml.Table.TableStyles.Medium16);
+
+                    var table = ws.Tables.GetFromRange(data);
+                    table.ShowFilter = true;                    
+                }
+
+                if (range != null)
+                {
+                    var t = ws.Tables.GetFromRange(range);
+                    t.ShowFilter = true;
+                }
+            }
+        }       
+    }
+}
---
+++ ExcelTemplates/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExcelTemplates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var date = GetValue(DateTime.Now.ToShortDateString());
+            var name = GetValue("John");
+            var test = GetValue("Test");
+            var names = GetValues("Name", 10).ToArray();
+            var tableItems = GetValues("TItem", 5).ToArray();
+
+            string pathIn = @"C:\Users\neoql\Downloads\1.xlsx";
+            string pathOut = @"C:\Users\neoql\Downloads\2.xlsx";
+
+            var xlTemplate = new ExcelTemplate(pathIn);
+
+            xlTemplate.SetField("Date", date);
+            xlTemplate.SetField("Name", name);
+            xlTemplate.SetField("Test", test);
+            xlTemplate.SetField("Names", names);
+            xlTemplate.SetField("TableItem", tableItems);
+
+            byte[] output = xlTemplate.GetAsByteArray();
+
+            File.WriteAllBytes(pathOut, output);
+        }
+
+        private static IEnumerable<string> GetValues(string name, int count)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                yield return $"{name} {i}";
+            }
+        }
+
+        private static string GetValue(string name)
+        {
+            return name;
+        }
+    }
+}
